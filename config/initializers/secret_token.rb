6eaf53ecceea0ec594f1896599147086be662cdc
Jonathan Wexler <Jwexman@gmail.com>
2014-02-20 11:29:47 -0500

    First commit of SimplyFollow.Me
    This is the basic rails generated files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimplyFollowMe::Application.config.secret_key_base = 'b39c7aaba0bdcd88bb98dd12ad452b22b6a7af8a6977991236bb1895ee008f2284b5812e39ca2a5773786e5c45c0cb0ce279d3e419299f43192577c7be004102'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SimplyFollowMe::Application.config.session_store :cookie_store, key: '_simply_follow_me_session'
